23746e09109821753e7cf7a489d9ebc86c5c24d5
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-18 14:35:30 +0100

    update

--- Model/ProductsModel.ts
+++ Model/ProductsModel.ts
@@ -1,42 +1,46 @@
 import mongoose from "mongoose";
 
+// product type structure 
 interface Iprod {
-	title: string;
-	desc: string;
-	price: number;
-	quantity: number;
-	category: string;
-	status: boolean;
+  title: string;
+  desc: string;
+  price: number;
+  quantity: number;
+  category: string;
+  status: boolean;
 }
 
+// extends to mongoose documents
 interface UserData extends Iprod, mongoose.Document {}
 
+//product schema
 const ProductSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-		},
-		desc: {
-			type: String,
-		},
+  {
+    title: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
 
-		price: {
-			type: Number,
-		},
+    price: {
+      type: Number,
+    },
 
-		quantity: {
-			type: Number,
-		},
+    quantity: {
+      type: Number,
+    },
 
-		category: {
-			type: String,
-		},
+    category: {
+      type: String,
+    },
 
-		status: {
-			type: Boolean,
-		},
-	},
-	{ timestamps: true },
+    status: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
 );
 
+// product model
 export default mongoose.model<UserData>("products", ProductSchema);
