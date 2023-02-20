dd0e43f90b9152a2ee3a9047d90ce29e4807ef72
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-20 15:32:16 +0100

    update

--- Controller/AllController.ts
+++ Controller/AllController.ts
@@ -6,144 +6,158 @@ import jwt from "jsonwebtoken";
 const router = express.Router();
 
 //get all users
-
 router.get("/users", async (req: Request, res: Response) => {
-	try {
-		const fetchUser = await UserModel.find();
-		return res.status(200).json({
-			message: "success",
-			data: fetchUser,
-		});
-	} catch (err) {
-		res.status(404).json({
-			message: "an error occured",
-		});
-	}
+  try {
+    const fetchUser = await UserModel.find();
+    return res.status(200).json({
+      message: "success",
+      data: fetchUser,
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: "an error occured",
+    });
+  }
+});
+
+//get one user
+router.get("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const grtOneUser = await UserModel.findById(req.params.id);
+    return res.status(200).json({
+      message: "success",
+      data: grtOneUser,
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: "an error occured",
+    });
+  }
 });
 
 // register a user
 router.post("/register", async (req: Request, res: Response) => {
-	try {
-		const { name, email, password } = req.body;
-		const createUser = await UserModel.create({
-			name,
-			email,
-			password,
-		});
-		return res.status(200).json({
-			message: "success",
-			data: createUser,
-		});
-	} catch (err) {
-		res.status(404).json({
-			message: "an error occured",
-		});
-	}
+  try {
+    const { name, email, password } = req.body;
+    const createUser = await UserModel.create({
+      name,
+      email,
+      password,
+    });
+    return res.status(200).json({
+      message: "success",
+      data: createUser,
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: "an error occured",
+    });
+  }
 });
 
 router.post("/register", async (req: Request, res: Response) => {
-	try {
-		const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-		// const salt = await bcrypt.genSalt(10);
-		// const hash = bcrypt.hash(password, salt);
-		const createUser = await UserModel.create({
-			name,
-			email,
-			password,
-			isAdmin: false,
-		});
-		return res.status(200).json({
-			message: "success",
-			data: createUser,
-			// token: jwt.sign(
-			// 	{ _id: createUser._id },
-			// 	"Ths-57aenrn-53q4yhnae-05q3ujn",
-			// 	{ expiresIn: "1d" },
-			// ),
-		});
-	} catch (err) {
-		res.status(404).json({
-			message: "an error occured",
-		});
-	}
+    // const salt = await bcrypt.genSalt(10);
+    // const hash = bcrypt.hash(password, salt);
+    const createUser = await UserModel.create({
+      name,
+      email,
+      password,
+      isAdmin: false,
+    });
+    return res.status(200).json({
+      message: "success",
+      data: createUser,
+      // token: jwt.sign(
+      // 	{ _id: createUser._id },
+      // 	"Ths-57aenrn-53q4yhnae-05q3ujn",
+      // 	{ expiresIn: "1d" },
+      // ),
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: "an error occured",
+    });
+  }
 });
 
 router.post("/login", async (req: Request, res: Response) => {
-	try {
-		const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-		const checkUser = await UserModel.findOne({ email: email });
+    const checkUser = await UserModel.findOne({ email: email });
 
-		if (checkUser) {
-			return res.status(200).json({
-				message: "success",
-				data: checkUser,
-			});
-		} else {
-			return res.status(200).json({
-				message: "user not found",
-			});
-		}
-	} catch (err) {
-		res.status(404).json({
-			message: "an error occured",
-		});
-	}
+    if (checkUser) {
+      return res.status(200).json({
+        message: "success",
+        data: checkUser,
+      });
+    } else {
+      return res.status(200).json({
+        message: "user not found",
+      });
+    }
+  } catch (err) {
+    res.status(404).json({
+      message: "an error occured",
+    });
+  }
 });
 
 // create produt
 
 router.post("/createProduct", async (req: Request, res: Response) => {
-	try {
-		const { title, desc, price, category, quantity, status } = req.body;
+  try {
+    const { title, desc, price, category, quantity, status } = req.body;
 
-		const creating = await ProductsModel.create({
-			title,
-			desc,
-			price,
-			category,
-			quantity,
-			status: true,
-		});
+    const creating = await ProductsModel.create({
+      title,
+      desc,
+      price,
+      category,
+      quantity,
+      status: true,
+    });
 
-		res.status(200).json({
-			message: "successfully created",
-			data: creating,
-		});
-	} catch (err) {
-		res.status(404).json({
-			message: "an error occured",
-		});
-	}
+    res.status(200).json({
+      message: "successfully created",
+      data: creating,
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: "an error occured",
+    });
+  }
 });
 
 //puschasing product
 router.patch(
-	"/purchaseProduct/:productID",
-	async (req: Request, res: Response) => {
-		try {
-			const { qty } = req.body;
+  "/purchaseProduct/:productID",
+  async (req: Request, res: Response) => {
+    try {
+      const { qty } = req.body;
 
-			// const getUser = await UserModel.findById(req.params.userID);
-			const getProducts = await ProductsModel.findById(req.params.productID);
-			console.log(getProducts);
+      // const getUser = await UserModel.findById(req.params.userID);
+      const getProducts = await ProductsModel.findById(req.params.productID);
+      console.log(getProducts);
 
-			if (getProducts!.quantity == 0) {
-				await ProductsModel.findByIdAndUpdate(getProducts!._id!, {
-					status: false,
-				});
-			} else {
-				await ProductsModel.findByIdAndUpdate(getProducts!._id!, {
-					quantity: getProducts?.quantity! - qty,
-				});
-			}
-		} catch (err) {
-			res.status(404).json({
-				message: "an error occured",
-			});
-		}
-	},
+      if (getProducts!.quantity == 0) {
+        await ProductsModel.findByIdAndUpdate(getProducts!._id!, {
+          status: false,
+        });
+      } else {
+        await ProductsModel.findByIdAndUpdate(getProducts!._id!, {
+          quantity: getProducts?.quantity! - qty,
+        });
+      }
+    } catch (err) {
+      res.status(404).json({
+        message: "an error occured",
+      });
+    }
+  }
 );
 
 export default router;
